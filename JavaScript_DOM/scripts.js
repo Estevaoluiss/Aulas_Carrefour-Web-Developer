feed996031ae7e983ff7da001daaa05cda84cc39
Estevão Luiz de siqueira nunes <102254216+Estevaoluiss@users.noreply.github.com>
2022-05-01 16:59:41 -0300

    'commit'

--- JavaScript_DOM/scripts.js
+++ JavaScript_DOM/scripts.js
@@ -1,20 +1,26 @@
+// cada function tem uma unica responsabilidade
+
 function changeMode() {
   changeClasses()
   changeText()
 }
 
+// o toggle vai adicionar ou tirar a class="dark-mode" na hora do click
 function changeClasses() {
-  button.classList.toggle(darkModeClass) // o toggle vai adicionar ou tirar a class="dark-mode" na hora do click
+  button.classList.toggle(darkModeClass)
   h1.classList.toggle(darkModeClass)
   body.classList.toggle(darkModeClass)
   footer.classList.toggle(darkModeClass)
 }
+
+// trocando os textos dpendendo de qual for o modo light ou dark
 function changeText() {
   const lighMode = 'Light Mode'
   const darkmode = 'Dark Mode'
 
-  if (button.classList.contains(darkModeClass)) {
-    button.innerHTML = lighMode
+  //vai ver se dentro do body ja tem a class list contains "darkModeClass"
+  if (body.classList.contains(darkModeClass)) {
+    button.innerHTML = lighMode //
     h1.innerHTML = darkmode + 'On'
     return
   }
@@ -23,10 +29,13 @@ function changeText() {
   h1.innerHTML = lighMode + 'On'
 }
 
+// criando variaveis para cada item
 const darkModeClass = 'dark-mode'
 const button = document.getElementById('mode-selector')
 const h1 = document.getElementById('page-title')
+
+// selecionando sem o "id" direto na tag o [0] é index do arry
 const body = document.getElementsByTagName('body')[0]
-const footer = document.getElementsByTagName('footer')[0]
+const footer = document.getElementsByTagName('footer')[0] // selecionando sem o "id" direto na tag
 
 button.addEventListener('click', changeMode)
